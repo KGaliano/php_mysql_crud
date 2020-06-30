949d8ffc3cb67746efcec82ad37d421536f991f8
Jaime Farfan NET <jfarfannet@gmail.com>
2020-06-30 14:33:42 -0500

    Primer commit

---
+++ pag1.php
@@ -0,0 +1,21 @@
+<html lang="es">
+	<head>
+		<title>Mantenimiento</title>
+        
+	</head>
+
+<body>
+
+<form action="pag2.php" method="post">
+	<input type="hidden" name="id" value="registro" />
+	
+Nombre  <input type="text" name="nombre" value="" /> <br>
+Apellido<input type="text" name="apellido" value="" /> <br>
+
+<button type="submit" >Enviar</button>
+
+</form>
+				
+</div>
+</body>
+</html>
---
+++ pag2.php
@@ -0,0 +1,20 @@
+<html lang="es">
+	<head>
+		<title>Agregar</title>
+        
+	</head>
+
+<body>
+<h1> AGREGAR </H1>
+<?php
+$nom = $_POST["nombre"];
+echo $nom; 
+echo "<br>"; 
+$ape = $_POST["apellido"];
+echo $ape;
+
+?>
+				
+</div>
+</body>
+</html>
